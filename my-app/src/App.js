924c33899d2c562ef3a1366e7bebbc6ce6adde21
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-03-08 08:12:48 +0000

    Add ContentHooks.js file to Apps.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
 import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
+// import Sidebar from "./components/Sidebar";
+// import NavBarForm from "./components/NavBarForm";
+// import Content from "./components/Content";
+import ContentHooks from "./components/ContentHooks";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarForm />
+      {/* <NavBarForm />
       <Sidebar />
-      <Content />
+      <Content /> */}
+      <ContentHooks />
     </div>
   );
 }
---
+++ my-app/src/components/ContentHooks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ContentHooks() {
+  return (
+    <div>Working?</div>
+  )
+}
+
+export default ContentHooks
+
